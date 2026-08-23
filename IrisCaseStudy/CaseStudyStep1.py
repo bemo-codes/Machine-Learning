9927d65263d68601f9e3aef4e05d92b69666ea65
bemo-codes <shivamxapp@gmail.com>
2026-08-23 23:32:31 +0530

    Load and display Iris dataset in CaseStudyStep1.py
    Added code to load and display the Iris dataset using pandas.

--- IrisCaseStudy/CaseStudyStep1.py
+++ IrisCaseStudy/CaseStudyStep1.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+Border = "-"*30
+
+#############################################
+# Step-1: Load the data set
+#############################################
+
+print(Border)
+print("Step-1: Load the data set")
+print(Border)
+
+DataPath = "iris.csv"               #Accessing iris.csv through relative path
+
+df = pd.read_csv(DataPath)          #df = Data frame i.e 2D array ex: an excel file
+
+print("Dataset loaded successfully")
+print("Initial entries from dataset are: ")
+print(df.head())
